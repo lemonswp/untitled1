470ef01dde48963b9c6495d4ddf441e7d0a95e31
poulwu <lemonswp@gmail.com>
2021-04-27 22:10:36 +0800

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer
+{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(AppConfiguration.class);
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication application = new SpringApplication(App.class);
+        application.setApplicationContextClass(AnnotationConfigWebApplicationContext.class);
+        SpringApplication.run(App.class, args);
+    }
+}
